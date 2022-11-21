c05fa358945124d6e0f8ee4655cd236f84412f09
Archaya007 <107406840+Archaya007@users.noreply.github.com>
2022-11-21 18:12:14 +0530

    4 files modified, 7 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { HttpClientModule } from '@angular/common/http';
-
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FlightComponent } from './flight/flight.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule],
-  declarations: [AppComponent, HelloComponent, FlightComponent],
-  bootstrap: [FlightComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule, ReactiveFormsModule],
+  declarations: [AppComponent, HelloComponent, FlightComponent, LoginComponent],
+
+  bootstrap: [LoginComponent],
 })
 export class AppModule {}
---
+++ src/app/login/login.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Login } from './login';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginService {
+  constructor(private httpClient: HttpClient) {}
+  public getAllLoginService() {
+    return this.httpClient.get('http://localhost:8080/login/');
+  }
+  public getLoginService(id) {
+    return this.httpClient.get('http://localhost:8080/login/' + id);
+  }
+  public deleteLoginService(id) {
+    return this.httpClient.delete('http://localhost:8080/login/' + id);
+  }
+  public createLoginService(login: Login) {
+    const headers = { 'content-type': 'application/json' };
+    return this.httpClient.post(
+      'http://localhost:8080/login/',
+      JSON.stringify(login),
+      {
+        headers: headers,
+      }
+    );
+  }
+  public updateLoginService(id, login: Login) {
+    const headers = { 'content-type': 'application/json' };
+    return this.httpClient.put(
+      'http://localhost:8080/login/' + String,
+      JSON.stringify(Login),
+      {
+        headers: headers,
+      }
+    );
+  }
+}
